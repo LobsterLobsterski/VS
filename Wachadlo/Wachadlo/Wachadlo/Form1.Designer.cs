c10dd5347d241ddb0ed5ca9dbd4ff26fd926c4de
LobsterLobsterski <100423134+LobsterLobsterski@users.noreply.github.com>
2022-06-08 16:38:37 +0200

    Add files via upload

---
+++ Wachadlo/Wachadlo/Wachadlo/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Wachadlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 539);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wachaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(469, 542);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(281, 542);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(58, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 545);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Waga kulki";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 544);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Długość linki";
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 1;
+            this.hScrollBar1.Location = new System.Drawing.Point(234, 601);
+            this.hScrollBar1.Maximum = 90;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(410, 19);
+            this.hScrollBar1.TabIndex = 6;
+            this.hScrollBar1.Value = 90;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(197, 603);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kąt";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(632, 493);
+            this.panel2.TabIndex = 10;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(371, 584);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "90°";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 630);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Wachadlo/Wachadlo/Wachadlo/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// θ'' = − g⁄R sin θ 
+/// θ(t) = θ0cos(sqrt(g/R)*t)
+/// </summary>
+namespace Wachadlo
+{
+
+    public partial class Form1 : Form
+    {
+        bool click = false;
+        double angle = 90, acc = 0, vel = 0;
+        double damp=0.995;//arbitralna wartość która sprawia że wahadało przestanie wachać po pewnym czasie
+        int length = 100, waga=0;
+        Bitmap buffer;
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            //musi sprawdzać bo inaczej program będzie próbował
+            //rysować nie zainiciowaną bitmape co prowadzi
+            //do błędu
+            if (timer1.Enabled == true)
+            {
+                //'nakłada' bitmape na panel
+                e.Graphics.DrawImageUnscaled(buffer, 0, 0);
+            }
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            ///może poruszać kulką może????
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            label4.Text = hScrollBar1.Value.ToString()+ "°";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //punkt z którego wychodzi linka
+            int originX = panel2.Width / 2;
+            int originY = panel2.Height / 10;
+            //punkt w którym znajduję się odważnik
+            int bobX = originX + (int)(Math.Sin(angle) * length);
+            int bobY = originY + (int)(Math.Cos(angle) * length);
+
+            //liczenie zmiany w kącie odważnika
+            acc = (-9.81 / length) * Math.Sin(angle);
+            vel += acc;
+            vel *= damp;
+            angle += vel;
+
+            //bitmapa która będzie 'nakładana' na panel
+            buffer = new Bitmap(panel2.Width, panel2.Height);
+            Graphics g = Graphics.FromImage(buffer);
+
+            //obiekty na bitmapie
+            g.DrawLine(Pens.Black, originX, originY, bobX, bobY);
+            g.DrawLine(Pens.Black, panel2.Width / 3, 0, originX, originY);
+            g.DrawLine(Pens.Black, 2*panel2.Width / 3, 0, originX, originY);
+            g.FillEllipse(Brushes.Black, originX - 5, originY - 5, 10, 10);
+            //waga odważnika nie wpływu na jego wachania ale chciałem żeby
+            //było to widoczne więc zwiększam jego wielkość
+            g.FillEllipse(Brushes.Blue, bobX - (15+waga/2), bobY - (15+waga/2), 30+waga, 30+waga);
+
+            panel2.Invalidate();
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //zmiana tekstu guzika
+            if (click)
+            {
+                button1.Text = "Wachaj";
+            }
+            else
+            {
+                button1.Text = "Zatrzymaj";
+            }
+            click = !click;
+
+            //inicializacja wartości
+            angle = hScrollBar1.Value * Math.PI / 180;
+            length = (int)numericUpDown1.Value;
+            waga = (int)numericUpDown2.Value;
+           
+            timer1.Enabled = !timer1.Enabled;
+
+        }
+    }
+}
